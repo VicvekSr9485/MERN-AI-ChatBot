72e1d0367a9cc774fdc57aa9e52f3d9069060e4b
VicvekSr9485 <olamideoso65@gmail.com>
2023-10-26 12:31:28 +0100

    [feats]: add a footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import Header from "./components/Header"
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
-
+import { useAuth } from "./context/AuthContext";
+import Footer from "./components/footer/Footer";
 function App() {
+  const auth = useAuth();
 
   return (
     <main>
@@ -15,11 +18,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<Chat />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
