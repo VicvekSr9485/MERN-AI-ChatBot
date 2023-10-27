e6a995287bc59051246287057dc00dbcbcc42511
VicvekSr9485 <olamideoso65@gmail.com>
2023-10-27 05:07:42 +0100

    [feats]: rewrite auth provider for better formatting

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,52 +1,57 @@
-import { ReactNode, createContext,useContext,useEffect,useState } from "react";
-import { loginUser } from "../helpers/api-communicator";
-
-type User = {
+import {
+    ReactNode,
+    createContext,
+    useContext,
+    useEffect,
+    useState,
+  } from "react";
+  import {
+    loginUser,
+  } from "../helpers/api-communicator";
+  
+  type User = {
     name: string;
     email: string;
-}
-
-type UserAuth = {
+  };
+  type UserAuth = {
     isLoggedIn: boolean;
     user: User | null;
     login: (email: string, password: string) => Promise<void>;
     signup: (name: string, email: string, password: string) => Promise<void>;
     logout: () => Promise<void>;
-}
-
-const AuthContext = createContext<UserAuth | null>(null);
-export const AuthProvider = ({ children }: { children: ReactNode}) => {
+  };
+  const AuthContext = createContext<UserAuth | null>(null);
+  
+  export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  
     useEffect(() => {
+      // fetch if the user's cookies are valid then skip login
 
     }, []);
-
     const login = async (email: string, password: string) => {
-        const data = await loginUser(email, password);
-        if (data) {
-            setUser({ email: data.email, name: data.name });
-            setIsLoggedIn(true)
-        }
+      const data = await loginUser(email, password);
+      if (data) {
+        setUser({ email: data.email, name: data.name });
+        setIsLoggedIn(true);
+      }
     };
-    
     const signup = async (name: string, email: string, password: string) => {
-
+     
     };
-
     const logout = async () => {
-
+    
     };
-
+  
     const value = {
-        user,
-        isLoggedIn,
-        login,
-        logout,
-        signup,
+      user,
+      isLoggedIn,
+      login,
+      logout,
+      signup,
     };
-    return <AuthContext.Provider value={value}></AuthContext.Provider>
-}
-
-export const useAuth = () => useContext(AuthContext);
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  };
+  
+  export const useAuth = () => useContext(AuthContext);
