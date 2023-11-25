26c73724326d970e63f5b2fdedc3ae05685efc0d
VicvekSr9485 <olamideoso65@gmail.com>
2023-11-25 15:28:28 +0100

    [feats]: add more functionalities

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const loginUser = async (email: string, password: string) => {
-  const res = await axios.post("/user/login", { email, password });
+  const res = await axios.post("https://easy-pink-tick-tux.cyclic.app/api/v1/user/login", { email, password });
   if (res.status !== 200) {
     throw new Error("Unable to login");
   }
@@ -13,7 +13,7 @@ export const signupUser = async (
   email: string,
   password: string
 ) => {
-  const res = await axios.post("/user/signup", { name, email, password });
+  const res = await axios.post("https://easy-pink-tick-tux.cyclic.app/api/v1/user/signup", { name, email, password });
   if (res.status !== 201) {
     throw new Error("Unable to Signup");
   }
@@ -22,7 +22,7 @@ export const signupUser = async (
 };
 
 export const checkAuthStatus = async () => {
-  const res = await axios.get("/user/auth-status");
+  const res = await axios.get("https://easy-pink-tick-tux.cyclic.app/api/v1/user/auth-status");
   if (res.status !== 200) {
     throw new Error("Unable to authenticate");
   }
@@ -31,7 +31,7 @@ export const checkAuthStatus = async () => {
 };
 
 export const sendChatRequest = async (message: string) => {
-  const res = await axios.post("/chat/new", { message });
+  const res = await axios.post("https://easy-pink-tick-tux.cyclic.app/api/v1/chat/new", { message });
   if (res.status !== 200) {
     throw new Error("Unable to send chat");
   }
@@ -40,7 +40,7 @@ export const sendChatRequest = async (message: string) => {
 };
 
 export const getUserChats = async () => {
-  const res = await axios.get("/chat/all-chats");
+  const res = await axios.get("https://easy-pink-tick-tux.cyclic.app/api/v1/chat/all-chats");
   if (res.status !== 200) {
     throw new Error("Unable to send chat");
   }
@@ -49,7 +49,7 @@ export const getUserChats = async () => {
 };
 
 export const deleteUserChats = async () => {
-  const res = await axios.delete("/chat/delete");
+  const res = await axios.delete("https://easy-pink-tick-tux.cyclic.app/api/v1/chat/delete");
   if (res.status !== 200) {
     throw new Error("Unable to delete chats");
   }
@@ -58,7 +58,7 @@ export const deleteUserChats = async () => {
 };
 
 export const logoutUser = async () => {
-  const res = await axios.get("/user/logout");
+  const res = await axios.get("https://easy-pink-tick-tux.cyclic.app/api/v1/user/logout");
   if (res.status !== 200) {
     throw new Error("Unable to delete chats");
   }
